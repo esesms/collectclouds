a93688d905d548c9c56a53949cc81de6a20b35b1
Sue <35784147+esesms@users.noreply.github.com>
2019-03-15 11:35:54 -0400

    Added some nltk tests

--- db_flavor_search.py
+++ db_flavor_search.py
@@ -1,11 +1,15 @@
 #Sue Huang
 #03/13/19
 #uses the food python library
+#drag the .py and .db files into env/lib/PythonX/site-packages/
 #https://think.cs.vt.edu/corgis/python/food/food.html
 
 
 
+
 import food
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
 #list_of_report is a list of dictionaries
 list_of_report = food.get_reports()
@@ -21,7 +25,9 @@ list_of_report = food.get_reports()
         
 for food in list_of_report:
     description = food["Description"].lower() #converts everything to lower case
-    print(description)
+    tokens = word_tokenize(description)
+    words = [word for word in tokens if word.isalpha()] #removes commas
+    print(words)
 
 
 #we will search for what "clouds taste like" in the twitter data and if something in the string is equivalent to portions of string from description, then we will count that as an ingredient for the ice cream
